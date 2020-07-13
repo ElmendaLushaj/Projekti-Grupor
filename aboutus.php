b0c5102ddfde34e0bc48e928196eed371c6baa58
KujteseLahi <63053562+KujteseLahi@users.noreply.github.com>
2020-07-13 22:20:51 +0200

    Add files via upload

--- aboutus.php
+++ aboutus.php
@@ -17,29 +17,38 @@
                 <button id="homebtn" type="menu"><img id="imge" src="languageicon.JPG"  ></button>
 
             </div>
+
             <div class="BoxiKryesor">
 
 
-                <div class="AboutUsBoxi">
-                    <h1 style="padding-bottom:1px; padding-left: 13%; font-family:'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;">
-                        ABOUT US</h1>
-                    <h4 style="color:black; font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;">
-                        Pocket Market is a new application,
-                        <br> which keeps you informed in real time
-                        <br> about the products and markets that you express interest.
-                        <br> For any ambiguity feel free to contact us.
-                    </h4>
-                    <button class="button">Send Email</button>
-                </div>
-                <div class="FOTOBox">
-                    <img src="logoja.png" style="width:550px; height:500px;padding-top: 25px; padding-bottom: 25px;">
-
-                </div>
-            </div>
-
-        </div>
+<div class="AboutUsBoxi">
+    <h1 style="padding-bottom:0px; padding-left: 70%; font-family:'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;">
+        ABOUT US</h1>
+    <h4 style="color:black; padding-left: 15%; font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;">
+        Pocket Market is a new application,
+        <br> which keeps you informed in real time about 
+        <br>  the products and markets that you express interest.
+        <br> For any ambiguity feel free to contact us.
+    </h4>
+        <div class="textarea">
+            <h3 style="padding-left:15%; font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;"> QUESTION AND CRITIC US</h3>
+            <textarea id="area" name="w3review" rows="10" cols="50">
+               
+                </textarea>
+    
+    <button class="button">Send</button>
     </div>
+</div>
+<div class="FOTOBox">
+    <img src="logoja.png" style="width:40vw; height:70vh;padding-top: 25px; padding-bottom: 10px;">
+
+</div>
+</div>
+
+</div>
+</div>
 </body>
+            
 <footer>
     <div class="footer">
         <div class="pckMrk" style="font-family: Calibri; font-size: 40px; ">pocketMarket</div>
