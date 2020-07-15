1e122a64cbeed4de4fefbb4462bfdef136ba6d78
Melina Lushaku <ml45784@ubt-uni.net>
2020-07-15 14:11:29 +0200

    Commit

--- productpage.php
+++ productpage.php
@@ -7,6 +7,8 @@ $produktet = $produkt->getP();
 <html>
 
 <head>
+<meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>pocketMarket-products</title>
     <link rel="stylesheet" href="productstyle.css">
 
